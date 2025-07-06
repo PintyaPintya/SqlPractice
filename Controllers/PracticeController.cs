de8eae3313df4f6d8a23f61b674e05699a6194aa
PintyaPintya <omkajale09@gmail.com>
2025-07-06 22:38:42 +0530

    Base project

---
+++ Controllers/PracticeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SqlQueryPractice.Models;
+
+namespace SqlQueryPractice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PracticeController : ControllerBase
+    {
+        private readonly SqlPracticeContext _context;
+
+        public PracticeController(SqlPracticeContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("Select")]
+        public async Task<ActionResult> Select()
+        {
+            var result = await _context.Students.ToListAsync();
+
+            return Ok(result);
+        }
+    }
+}
--- Models/BatchStudent.cs
+++ Models/BatchStudent.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace SqlQueryPractice.Models;
+﻿namespace SqlQueryPractice.Models;
 
 public partial class BatchStudent
 {
